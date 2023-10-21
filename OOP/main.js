76881c889518ea5d0502b7baaac66bc602947c5e
Shaikh Muhammad Saqib <saqib.kareem.gmb@gmail.com>
2023-10-21 21:04:39 +0500

    update

---
+++ OOP/main.js
@@ -0,0 +1,2 @@
+const __filename;
+export {};
---
+++ OOP/main.ts
@@ -0,0 +1,7 @@
+import inquirer from "inquirer";
+import path from "path";
+import  fs from "fs";
+import { pathToFileURL } from "url";
+
+const __filename
+
